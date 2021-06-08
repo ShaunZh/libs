5212363b23eabbc406a2bf3d4c331899625fbecf
shaunZh <308826842@qq.com>
2021-06-08 10:00:10 +0800

    update: 简化整理代码

--- myPromise/myPromise.js
+++ myPromise/myPromise.js
@@ -3,7 +3,7 @@
  * @Author: Hexon
  * @Date: 2021-06-03 13:56:22
  * @LastEditors: Hexon
- * @LastEditTime: 2021-06-07 17:58:50
+ * @LastEditTime: 2021-06-08 09:59:54
  */
 
 // 原生Promise实现
@@ -38,6 +38,8 @@ class MyPromise {
   onRejectedCallbacks = []
 
 
+
+
   resolve = (val) => {
     if (this.state === PENDING) {
       this.state = FULFILLED
@@ -65,7 +67,7 @@ class MyPromise {
     onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
 
     const promise2 = new MyPromise((resolve, reject) => {
-      if (this.state === FULFILLED) {
+      const fulfilledMicrotask = () => {
         queueMicrotask(() => {
           try {
             let x = onFulfilled(this.value)
@@ -75,7 +77,8 @@ class MyPromise {
             reject(error)
           }
         })
-      } else if (this.state === REJECTED) {
+      }
+      const rejectedMicrotask = () => {
         queueMicrotask(() => {
           try {
             let x = onRejected(this.value)
@@ -84,27 +87,14 @@ class MyPromise {
             reject(error)
           }
         })
+      }
+      if (this.state === FULFILLED) {
+        fulfilledMicrotask()
+      } else if (this.state === REJECTED) {
+        rejectedMicrotask()
       } else if (this.state === PENDING) {
-        this.onFulfilledCallbacks.push(() => {
-          queueMicrotask(() => {
-            try {
-              let x = onFulfilled(this.value)
-              this.resolvePromise(promise2, x, resolve, reject)
-            } catch (error) {
-              reject(error)
-            }
-          })
-        })
-        this.onRejectedCallbacks.push(() => {
-          queueMicrotask(() => {
-            try {
-              let x = onRejected(this.value)
-              this.resolvePromise(promise2, x, resolve, reject)
-            } catch (error) {
-              reject(error)
-            }
-          })
-        })
+        this.onFulfilledCallbacks.push(fulfilledMicrotask)
+        this.onRejectedCallbacks.push(rejectedMicrotask)
       }
     })
     // 要实现then的链式调用，因此，必须返回一个promise
@@ -113,60 +103,86 @@ class MyPromise {
 
   resolvePromise = (promise2, p, resolve, reject) => {
     if (promise2 === p) {
-      console.log('等于')
       return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
     }
-    // 如果p是是promise
     if (p instanceof MyPromise) {
+      // 执行 x，调用 then 方法，目的是将其状态变为 fulfilled 或者 rejected
+      // x.then(value => resolve(value), reason => reject(reason))
+      // 简化之后
       p.then(resolve, reject)
     } else {
       resolve(p)
     }
+  }
 
+  static resolve(parameter) {
+    if (parameter instanceof MyPromise) {
+      return parameter
+    }
+
+    return new MyPromise(resolve => {
+      resolve(parameter)
+    })
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason)
+    })
   }
 }
 
-const testMyPromise = new MyPromise((resolve, reject) => {
-  // 添加setTimeout后没有打印出resolve success，这是因为主线程立即执行，setTimeout是异步代码，属于macro task，
-  // then会马上执行，此时state状态还是Pending，而在then函数中并未等待Pending状态，因此，没有打印信息
-  // setTimeout(() => {
-  // throw new Error('test error')
-  // resolve('success')
-  // }, 2000)
-  reject('err')
-})
-
-// const p1 = testMyPromise.then(value => {
-//   console.log(1)
-//   console.log('resolve', value)
-//   return p1
+// const testMyPromise = new MyPromise((resolve, reject) => {
+//   // 添加setTimeout后没有打印出resolve success，这是因为主线程立即执行，setTimeout是异步代码，属于macro task，
+//   // then会马上执行，此时state状态还是Pending，而在then函数中并未等待Pending状态，因此，没有打印信息
+//   // setTimeout(() => {
+//   // throw new Error('test error')
+//   // resolve('success')
+//   // }, 2000)
+//   reject('err')
 // })
 
-// 测试不传then参数
-testMyPromise.then().then().then(value => console.log(value))
+// // const p1 = testMyPromise.then(value => {
+// //   console.log(1)
+// //   console.log('resolve', value)
+// //   return p1
+// // })
+
+// // 测试不传then参数
+// testMyPromise.then().then().then(value => console.log(value))
 
 
-// 运行的时候会走reject
-testMyPromise.then(value => {
-  console.log(1)
-  console.log('resolve ', value)
-  throw new Error('then error')
-}, reason => {
-  console.log(2)
-  console.log(reason.message)
-}).then(value => {
-  console.log(3)
-  console.log('resolve ', value)
-}, error => {
-  console.log(4)
-  console.log(error.message)
-  throw new Error('catch error')
-}).then(value => {
-  console.log(5)
-  console.log(value)
-}, error => {
-  console.log(6)
-  console.log(error.message)
-})
+// MyPromise.resolve().then(() => {
+//   console.log(0);
+//   return MyPromise.resolve(4);
+// }).then((res) => {
+//   console.log(res)
+// })
+
+// MyPromise.reject('test error').then(v => console.log('v', v), e => console.error(e))
+
+
+// // 运行的时候会走reject
+// testMyPromise.then(value => {
+//   console.log(1)
+//   console.log('resolve ', value)
+//   throw new Error('then error')
+// }, reason => {
+//   console.log(2)
+//   console.log(reason.message)
+// }).then(value => {
+//   console.log(3)
+//   console.log('resolve ', value)
+// }, error => {
+//   console.log(4)
+//   console.log(error.message)
+//   throw new Error('catch error')
+// }).then(value => {
+//   console.log(5)
+//   console.log(value)
+// }, error => {
+//   console.log(6)
+//   console.log(error.message)
+// })
 
 
